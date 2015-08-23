d38880e18b5cba055e114e638913f64003917373
Oksana Vynokurova <oksanka.vynokurova@gmail.com>
2015-08-24 00:14:44 +0300

    Added HW_task3, HW_task4.

---
+++ HW_task3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HW1_task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal pi = 3.141592653m;
+            decimal e = 2.7182818284590452m;
+            System.Console.WriteLine(pi);
+            System.Console.WriteLine(e);
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ HW_task4/HW_task4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HW1_task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "\nмоя строка 1";
+            string s2 = "\tмоя строка 2";
+            string s3 = "\aмоя строка 3";
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+            Console.ReadKey();
+        }
+    }
+}
